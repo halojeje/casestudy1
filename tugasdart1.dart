d86d92982d0ff6dbecb34f8b4efdf69f39e92f55
Jeihan <jeihan.azib@gmail.com>
2026-07-20 07:22:44 +0700

    tugas dart1

--- tugasdart1.dart
+++ tugasdart1.dart
@@ -1,27 +1,27 @@
 void main() {
-    String nama = "Jeihan"}
+    String nama = "Jeihan";
     int usia = 25;
     print(nama);
     print("Nama = $nama, Usia = $usia");
 
-    double tinggi badan = 175;
+    double tinggiBadan = 175;
 
     // status aktif keanggotaan
-    bool status aktif = true;
+    bool statusAktif = true;
 
-    List daftar buku favorit = ["Haruki Minami - 1972","Dan Brown","Little World"];
-    print(daftar buku favorit);
-    print(daftar buku favorit)[0];
-    print(daftar buku favorit)[1];
+    List daftarBukuFavorit = ["Haruki Minami - 1972","Dan Brown","Little World"];
+    print(daftarBukuFavorit);
+    print(daftarBukuFavorit[0]);
+    print(daftarBukuFavorit[1]);
     /*Ini untuk list daftar buku*/
 
-    Map<String, dynamic> biodata anggota = {
-        "Alamat" : Jakarta Timur,
-        "Profesi" : Desainer,
+    Map<String, dynamic> biodataAnggota = {
+        "Alamat" : "Jakarta Timur",
+        "Profesi" : "Desainer Grafis",
     /*Ini bagian alamat dan profesi */
     };
     
-    print(biodata anggota);
-    print(biodata anggota["Alamat"]);
-    print(biodata anggota["Profesi"]);
+    print(biodataAnggota);
+    print(biodataAnggota["Alamat"]);
+    print(biodataAnggota["Profesi"]);
 } 
